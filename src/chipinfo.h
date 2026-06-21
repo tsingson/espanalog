09967e2a96a70ec9376b16facc76761977d455bf
qinshen <tsingson@gmail.com>
2026-06-21 19:55:27 +0800

    add chip info

---
+++ src/chipinfo.h
@@ -0,0 +1,14 @@
+//
+// Created by tsingson on 2026/6/21.
+//
+
+#ifndef LED_BRIGHTNESS_CONTROL_CHIPINFO_H
+#define LED_BRIGHTNESS_CONTROL_CHIPINFO_H
+
+
+
+
+void print_chip_info();
+
+
+#endif // LED_BRIGHTNESS_CONTROL_CHIPINFO_H
